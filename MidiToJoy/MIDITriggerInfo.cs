0a9b544fa74127e57ef8177b555880ff9cdc698b
shigenobu <shigenobu314@yahoo.co.jp>
2020-09-06 22:57:40 +0900

    MIDITriggerInfo作成

--- MidiToJoy/MIDITriggerInfo.cs
+++ MidiToJoy/MIDITriggerInfo.cs
@@ -8,6 +8,26 @@ namespace MidiToJoy
 {
 	class MIDITriggerInfo
 	{
+		public MIDITriggerInfo(int channel, MIDITriggerType type, int dataByte1)
+		{
+			Channel = channel;
+			Type = type;
+			DataByte1 = dataByte1;
+		}
 
+		/// <summary>
+		/// チャンネル １始まり
+		/// </summary>
+		public int Channel { get; set; }
+
+		/// <summary>
+		/// タイプ
+		/// </summary>
+		public MIDITriggerType Type { get; set; }
+
+		/// <summary>
+		/// データバイト１
+		/// </summary>
+		public int DataByte1 { get; set; }
 	}
 }
